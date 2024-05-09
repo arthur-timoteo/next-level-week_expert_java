afe1b809ae077b914308a00aa7f113bae02705d8
arthur-timoteo <arthur.timoteo@gmail.com>
2024-05-08 22:30:52 -0300

    feat: Create database connection and some endpoints

--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifiIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifiIfHasCertificationUseCase.java
@@ -1,14 +1,21 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifiIfHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifiIfHasCertificationUseCase {
     
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifiIfHasCertificationDTO dto){
-        if(dto.getEmail().equals("arthur@bol.com") && dto.getTechnology().equals("JAVA")){
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+
+        if(!result.isEmpty()){
             return true;
         }
 
